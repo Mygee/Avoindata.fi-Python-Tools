bc49bd78c502ea62aba3e3da91d2731c48ba3945
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-11-08 09:31:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Avoindata.fi-Python-Tools',
+    version='',
+    packages=['scripts', 'scripts.prh'],
+    url='https://github.com/Mygee/Avoindata.fi-Python-Tools',
+    license='MIT',
+    author='Jari-Pekka Voutilainen',
+    author_email='',
+    description='',
+    install_requires=[
+        "requests",
+        "beautifulsoup4"
+    ]
+)
