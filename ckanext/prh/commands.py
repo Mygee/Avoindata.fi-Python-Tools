747100eb9464b6496bffba3cac4aca4891cfbabd
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-02-17 14:07:30 +0200

    Add notifications and clear command to script

--- ckanext/prh/commands.py
+++ ckanext/prh/commands.py
@@ -1,3 +1,10 @@
+import os
+import shutil
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path  # python 2 backport
+
 from ckan.lib.cli import (
     load_config,
     paster_click_group,
@@ -28,9 +35,32 @@ prh_group = paster_click_group(
 @click.pass_context
 def fetch(ctx, base_dir, year, start_from_beginning, package_id, config):
     load_config(config or ctx.obj['config'])
-    PRHData().get_prh_data(base_directory=base_dir, year=year,
-                           start_from_beginning=start_from_beginning)
-    filename = make_csv_of_prh_data(base_directory=base_dir)
 
-    from scripts.prh.upload_to_ckan import upload_to_ckan
-    upload_to_ckan(package_id, filename)
+    if Path(os.path.join(base_dir, 'data', 'json', 'prh_data', 'all_done.txt')).is_file():
+        print("Nothing to do")
+
+    else:
+        PRHData().get_prh_data(base_directory=base_dir, year=year,
+                               start_from_beginning=start_from_beginning)
+        filename = make_csv_of_prh_data(base_directory=base_dir)
+        from scripts.prh.upload_to_ckan import upload_to_ckan
+        upload_to_ckan(package_id, filename)
+
+        with open(os.path.join(base_dir, 'data', 'json', 'prh_data', 'all_done.txt'), 'w') as done_file:
+            done_file.write("done")
+            done_file.close()
+
+@prh_group.command(
+    u'clear',
+    help="Clears fetched data"
+)
+@click.argument(u'BASE_DIR', nargs=1, required=True)
+@click.help_option(u'-h', u'--help')
+@click_config_option
+@click.pass_context
+def clear(ctx, base_dir, config):
+    load_config(config or ctx.obj['config'])
+    try:
+        shutil.rmtree(os.path.join(base_dir, 'data', 'json', 'prh_data'))
+    except OSError:
+        print("Directory does not exist")
--- ckanext/prh/scripts/prh/upload_to_ckan.py
+++ ckanext/prh/scripts/prh/upload_to_ckan.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from ckan.lib import mailer
+import ckan.plugins.toolkit as t
+
 from ckanapi import LocalCKAN, ValidationError
 
 registry = LocalCKAN()
@@ -6,6 +11,28 @@ registry = LocalCKAN()
 def upload_to_ckan(package_id, filename):
 
     try:
-        registry.action.resource_create(package_id=package_id, upload=open(filename, 'rb'))
+        resource = registry.action.resource_create(package_id=package_id, upload=open(filename, 'rb'))
+
+        email_notification_recipients = t.aslist(t.config.get('ckanext.prh_tools.mail_recipients', ''))
+        site_title = t.config.get('ckan.site_title', '')
+        today = datetime.now().date().isoformat()
+
+        msg = '%(site_title)s - PRH data uploaded %(today)s\n\n%(status)s' % {
+            'site_title': site_title,
+            'today': today,
+            'status': "New data available in https://www.avoindata.fi/data/dataset/%s/resource/%s" % (package_id, resource.get('id'))
+        }
+
+        for recipient in email_notification_recipients:
+            email = {'recipient_name': '',
+             'recipient_email': recipient,
+             'subject': '%s - PRH data uploaded %s' % (site_title, today),
+             'body': msg}
+
+            try:
+                mailer.mail_recipient(**email)
+            except mailer.MailerException as e:
+                print 'Sending prh data notification to %s failed: %s' % (recipient, e)
+
     except ValidationError as e:
         print("Resource patch failed: %s" % e.error_summary)
